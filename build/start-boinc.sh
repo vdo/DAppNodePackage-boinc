95e3cb5ba84faadb4e29ca1ff592ad6687e5d27f
vdo <vdo@greyfaze.net>
2020-03-16 17:45:41 +0100

    Use wizard

--- build/start-boinc.sh
+++ build/start-boinc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Configure the GUI RPC
-echo $BOINC_GUI_RPC_PASSWORD >/var/lib/boinc/gui_rpc_auth.cfg
-echo $BOINC_REMOTE_HOST >/var/lib/boinc/remote_hosts.cfg
+echo ${RPC_PASSWORD} >/var/lib/boinc/gui_rpc_auth.cfg
+echo ${BOINC_REMOTE_HOST} >/var/lib/boinc/remote_hosts.cfg
 
-# Run BOINC. Full path needs for GPU support.
-exec /usr/bin/boinc $BOINC_CMD_LINE_OPTIONS
+# Run BOINC command to login to project, in background
+sleep 10 && boinccmd --host localhost \
+    --passwd ${RPC_PASSWORD} \
+    --project_attach ${PROJECT_URL} \
+    ${API_KEY} &
+# Run BOINC client
+/usr/bin/boinc ${CMDLINE_OPTIONS}
